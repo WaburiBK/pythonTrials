a3dc9681fe8daebff3e87408a36ebc8c2d085c70
WaburiBK <84006929+WaburiBK@users.noreply.github.com>
2021-07-27 07:02:13 +0300

    Function Examples
    Here I learnt the general applications of using functions(aka procedures). Functions can be passed with arguments and called later on using 'return'.
    When called through a print statement, the interpreter jumps back to the main body of the code and assigns the value of the print statement to the argument. E.g print rest_of_string("audacity") - the string audacity is assigned to the argument 's'. Thereafter, the interpreter jumps back to the return statement and executes the value of s[1:]

---
+++ Function Examples.py
@@ -0,0 +1,50 @@
+def say_hello(name):
+    greeting = "Hello " + name + "!"
+    return greeting
+
+print say_hello("Miriam")
+print say_hello("Andy")
+
+#def say_hello():
+    #return "Hello!"
+
+#print say_hello()
+
+#def add_two_numbers(number_1, number_2):
+    #return number_1 + number_2
+
+#print add_two_numbers(4, 3)
+#print add_two_numbers(2, 6)
+#print add_two_numbers(0, 9)
+
+#def subtract_two_numbers(number_1, number_2):
+    #return number_1 + number_2
+
+#print subtract_two_numbers(4, 3)
+
+def say_hello(name):
+    greeting = "Hello" + name + "!"
+    return greeting
+
+print say_hello(" Brian")
+print say_hello(" Wachira")
+
+def add_two_numbers(number_1, number_2):
+    return number_1 + number_2
+
+print add_two_numbers(3, 4)
+
+def rest_of_string(s):
+    #s = "audacity"
+    return s[1:]
+
+print rest_of_string('audacity')
+
+#def driving_to_work(speed):
+#speed = "distance" / "time"
+#distance = 10
+#time = 2
+#return speed 
+    
+#print driving_to_work("Monday Commute")
+
